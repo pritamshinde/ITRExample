ef2a7e0e40427216bce3aecdd5c2b61d2ad66c38
Pritam26 <shinde.pritam26@gmail.com>
2023-07-08 09:40:28 +0530

    Added this feature.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("I am doing email chanegs so all the email changes are in this branch.!");
